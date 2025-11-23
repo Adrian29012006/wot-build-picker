4d88771869edc396a8783fd9f609d5e190fd5c79
Adrian <theadrian2901@gmail.con>
2025-11-23 21:16:14 +0200

    Add Railway domain to ALLOWED_HOSTS

--- wotbuilds/settings.py
+++ wotbuilds/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = ["*", ".railway.app"]
+ALLOWED_HOSTS = [
+    "*",
+    ".railway.app",
+    "wot-build-picker-production.up.railway.app",
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
