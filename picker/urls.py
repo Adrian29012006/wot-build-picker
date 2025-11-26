07a93a110920cc8568da64af6eed4c52101f1c50
Adrian <theadrian2901@gmail.con>
2025-11-26 03:53:00 +0200

    Disable maps urls

--- picker/urls.py
+++ picker/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Главная страница
-    path('', views.index, name='index'),
-
-    # Карты
-    path('maps/', views.maps_list, name='maps_list'),
-    path('maps/<slug:slug>/', views.map_detail, name='map_detail'),
+    path("", views.index, name="index"),  # или твоя главная функция
 ]
